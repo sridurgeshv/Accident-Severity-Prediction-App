4a6e653263f0d6a423afe068bbfaa70da075752c
sridurgeshv <60750509+sridurgeshv@users.noreply.github.com>
2023-07-31 11:22:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -103,7 +103,7 @@ with b:
 
 
 # description about the project and code files       
-st.subheader("🧾Description:")
+st.subheader("★Description:")
 st.text("""This data set is collected from Addis Ababa Sub-city police departments for master's research work. 
 The data set has been prepared from manual records of road traffic accidents of the year 2017-20. 
 All the sensitive information has been excluded during data encoding and finally it has 32 features and 12316 instances of the accident.
@@ -112,7 +112,7 @@ Then it is preprocessed and for identification of major causes of the accident b
 
 st.markdown("Source of the dataset: [Click Here](https://www.narcis.nl/dataset/RecordID/oai%3Aeasy.dans.knaw.nl%3Aeasy-dataset%3A191591)")
 
-st.subheader("🧭 Problem Statement:")
+st.subheader("★Problem Statement:")
 st.text("""The target feature is Accident_severity which is a multi-class variable. 
 The task is to classify this variable based on the other 31 features step-by-step by going through each day's task. 
 The metric for evaluation will be f1-score
